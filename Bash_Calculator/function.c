94400ce6f72d2c44e8fca7cc7d7783cec4ebd6cb
Riddhi Tharewal <69417801+RiddhiTharewal@users.noreply.github.com>
2021-12-24 18:54:57 +0530

    Add files via upload

---
+++ Bash_Calculator/function.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "function.h"
+
+//Function to equal the digits after decimal point.
---
+++ Bash_Calculator/function.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_H_INCLUDED
+#define FUNCTION_H_INCLUDED
+
+
+
+
+
+#endif // FUNCTION_H_INCLUDED
